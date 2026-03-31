dc7cb2320cd36d72b51234998a2cbbdf06ece6fb
Joe <shengzhe.zhou@gmail.com>
2026-04-01 01:05:25 +1100

    style: initial project-wide format with prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Colors } from "@/utils/Colors";
+import { Stack } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          title: "AI Image Generator",
+          headerStyle: { backgroundColor: Colors.background },
+        }}
+      />
+      <View style={styles.container}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: Colors.background,
+  },
+});
